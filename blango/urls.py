f23c1e90d694578bd45d77bab241416bd4d70ef6
Victor Banerjee <recervictory@gmail.com>
2022-04-22 08:40:27 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 '''{ to view tim zone on command line}
