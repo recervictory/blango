afc693118c45c9cc683a347dc07efa4b649060ef
Victor Banerjee <recervictory@gmail.com>
2022-04-25 13:43:45 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,6 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
+
+def post_table(request):
+    return render(request, "blog/post-table.html")   
