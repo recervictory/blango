dcf72b420a6c1f96737293d4d5c9ced81cc5fece
Victor Banerjee <recervictory@gmail.com>
2022-05-23 14:14:26 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
 
-# Register Tags
-admin.site.register(Tag)
 
-# Register Post
+
+# Admin View Change
 class PostAdmin(admin.ModelAdmin):
   prepopulated_feilds = {'slug': ('title', )}
   list_display = ('slug', 'published_at')
 
+
+# Register Models
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 from django.conf import settings # for AUTH_USER_MODEL
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
 # Class One tag have many post and one post have many tag : Relationship
 # ManyToMany
@@ -11,6 +14,19 @@ class Tag(models.Model):
 
   def __str__(self):
     return self.value
+
+
+
+
+class Comment(models.Model):
+  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.TextField()
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type","object_id")
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
+
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -22,6 +38,7 @@ class Post(models.Model):
   summary = models.TextField(max_length=500)
   content = models.TextField()
   tags = models.ManyToManyField(Tag, related_name="posts")
+  comments = GenericRelation(Comment)
 
   def __str__(self):
     return self.title
