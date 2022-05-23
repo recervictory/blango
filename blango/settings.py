4e909c78ba59a3821eb0d12ea6ba52626c3bea66
Victor Banerjee <recervictory@gmail.com>
2022-05-23 15:29:19 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -19,6 +19,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -68,7 +69,8 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Templates Path Added
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
