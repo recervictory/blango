f6f51682871ca10d4b148471e214a6d66e311890
Victor Banerjee <recervictory@gmail.com>
2022-05-24 06:56:53 +0000

    Format HTML

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,30 +3,30 @@ from django import template
 # ---- Safe for Cross site Scripting ----
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+# ---- Format html ----
+from django.utils.html import format_html
 
 
 user_model = get_user_model()
 register = template.Library()
 
 
-
-@register.filter(name="author_details")
+@register.filter
 def author_details(author):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
 
     if author.first_name and author.last_name:
-        name = escape(f"{author.first_name} {author.last_name}")
+        name = f"{author.first_name} {author.last_name}"
     else:
-        name = escape(f"{author.username}")
+        name = f"{author.username}"
 
     if author.email:
-        email = escape(author.email)
-        prefix = f'<a href="mailto:{email}">'
-        suffix = "</a>"
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
     else:
         prefix = ""
         suffix = ""
 
-    return mark_safe(f"{prefix}{name}{suffix}")
+    return format_html('{}{}{}', prefix, name, suffix)
