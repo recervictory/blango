107ce6d789346d02ff0e3c34905b7d547c5be66a
Victor Banerjee <recervictory@gmail.com>
2022-05-24 13:57:10 +0000

    Simple Logging

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,15 @@ from django.shortcuts import render, get_object_or_404
 # comment forms
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+# logging
+import logging
+logger = logging.getLogger(__name__) # name is the module name
+
 
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  # Logging
+  logger.debug("Got %d posts", len(posts))
   return render(request, 'blog/index.html', {'posts':posts})
 
 def post_detail(request, slug):
@@ -23,6 +29,8 @@ def post_detail(request, slug):
           comment.content_object = post
           comment.creator = request.user
           comment.save()
+          # Logging
+          logger.info("Created comment on Post %d for user %s", post.pk, request.user)
           return redirect(request.path_info)
       else:
         comment_form = CommentForm()
